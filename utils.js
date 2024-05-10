dcdab4e598d0109a640bf37633f9b3af375de587
Shenhav <75911370+ShenhavBarziv@users.noreply.github.com>
2024-05-10 14:45:17 +0300

    FIxed findLocalIpAddress

--- utils.js
+++ utils.js
@@ -2,22 +2,14 @@ const os = require("os");
 const qr = require("qrcode");
 
 function findLocalIpAddress() {
-  const nets = os.networkInterfaces();
-  const results = Object.create(null);
-
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
-      if (net.family === familyV4Value && !net.internal) {
-        if (!results[name]) {
-          results[name] = [];
-        }
-        results[name].push(net.address);
-      }
-    }
-  }
-
-  return results["Wi-Fi"][0]; // Assuming Wi-Fi network is used
+  var ip = "0.0.0.0";
+  var ips = os.networkInterfaces();
+  Object.keys(ips).forEach(function (_interface) {
+    ips[_interface].forEach(function (_dev) {
+      if (_dev.family === "IPv4" && !_dev.internal) ip = _dev.address;
+    });
+  });
+  return ip;
 }
 
 async function generateQRCode(url) {
